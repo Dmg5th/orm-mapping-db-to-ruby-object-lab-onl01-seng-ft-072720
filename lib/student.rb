7f2f1030560b8905d12e3f92b54b896d2950b63f
Daniel Gould <dmgould310@gmail.com>
2020-08-26 14:21:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students 
      SQL
-     DB[:conn].execute(sql, name).map do |row|
+     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end 
   end
