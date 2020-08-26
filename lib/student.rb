9c004168fd80c83040a2c8f6433232a473ed4b18
Daniel Gould <dmgould310@gmail.com>
2020-08-26 14:20:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,15 +49,17 @@ class Student
     end 
   end 
   
-  def self.all_students_in_grade_X(x)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
     SELECT * FROM students 
     WHERE grade = ?
     SQL
-    DB[:conn].execute(sql,x).map do |row|
+    DB[:conn].execute(sql,grade).map do |row|
     self.new_from_db(row)
     end 
   end 
+  
+
     
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
